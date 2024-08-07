83625dc572eb9cb34ba3f5853d936189dd2d2d43
sabiopescador <temoteoprimo@gmail.com>
2024-08-07 13:54:38 -0300

    desafio 32

--- aulas_e_exercícios/ano_bissexto.py
+++ aulas_e_exercícios/ano_bissexto.py
@@ -1,1 +1,8 @@
 print('vamos ver quais métodos você usaria para saber o ano bissexto: ')
+
+ano = int(input('ANO: '))
+
+if ano % 4 == 0:
+    print('O ANO {} É BISSEXTO! :D'.format(ano))
+else:
+    print('O ANO {} NÃO É BISSEXTO :F'.format(ano))
