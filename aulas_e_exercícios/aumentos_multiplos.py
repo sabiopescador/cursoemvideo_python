1e61f0ad9a54325559c8abd4460eef9efa188a77
sabiopescador <temoteoprimo@gmail.com>
2024-08-09 00:57:23 -0300

    desafio 34

--- aulas_e_exercícios/aumentos_multiplos.py
+++ aulas_e_exercícios/aumentos_multiplos.py
@@ -1,5 +1,8 @@
 salario = float(input('Salário do funcionário R$: '))
 
-aumento = salario + (salario * 10/100)
+if salario > 1250.00:
+    aumento = salario + (salario * 10/100)
+else:
+    aumento = salario + (salario * 15/100)
 
 print('Quem ganhava {} parará a ganhar {}'.format(salario, aumento))
